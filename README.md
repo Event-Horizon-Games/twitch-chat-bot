c06eb20754a51c5f661969e4f9f5470aa598203c
Gravity <bazookaforreal@gmail.com>
2022-07-20 21:21:22 -0400

    move from personal repo

---
+++ README.md
@@ -0,0 +1,71 @@
+![GitHub all releases](https://img.shields.io/github/downloads/gravity-codes/stream-twitch-bot/total)
+
+# Twitch Stream Bot
+
+## Disclaimer
+
+Some pretty shitty things can be done with bots, since they are simply Twitch users that can join any channel without permission from the streamer. I wish I didn't have to say this but it's needed. This applies to any bot someone creates, but since I'm creating this walkthrough I feel especially obligated to give this warning. **ONLY JOIN THE BOT TO A CHANNEL IN WHICH YOU HAVE EXPLICIT PERMISSION TO DO SO, OR KEEP IT IN YOUR OWN CHANNEL.** Please just use your common sense and use this for a fun side project and nothing more. Keep it focues on fun and making streams better, and don't use it as a weapon. I can't do a whole lot, but if I find someone uses my guide for nefarious purposes I'll do whatever I can to fuck them up. Also if a streamer on the recieving end finds this page please let me know so I can help how ever I can.
+
+**Put simply, a chat bot is just a user so anyone dealing with a malicious bot can just ban it from their channel.**
+
+## Required tools
+
+**Install these before doing anything else!**
+
+- [NodeJS](https://nodejs.org/en/download/)
+- [MariaDB](https://mariadb.org/download/?t=mariadb&p=mariadb&r=10.8.3&os=windows&cpu=x86_64&pkg=msi&m=gigenet) *Use all default settings to include HeidiSQL*
+
+<figure>
+  <img src="https://i.imgur.com/ASyuIM7.png" alt="Image of Instructions" title="Proper Settings" width="60%"/>
+  <figcaption><i>Download options should match the image above.</i></figcaption>
+</figure>
+
+***Since a newer version may be release depending on when you read this, select whatever version has the highest version number with no letters.***
+
+I won't be teaching these tools here, but there is plenty of information out there as they are pretty standard tools. I will leave a link to the ones I used to get familiar. I recommened leaving every install on default, unless you know what you're doing. It will save trouble later on. Another important reminder is to remember what you set the user and pssword to for MariaDB. I made the mistake once of forgetting and it is not fun to fix. I think the common thing to do especially for a locally hosted DB is to set both to root. This will be inscure though so just be warned.
+
+- A good guide for initial setup of MariaDB can be found [here](https://www.mariadbtutorial.com/getting-started/install-mariadb/).
+- HeidiSQL provides basic instructions on how to connect to a MariaDB server [here](https://www.heidisql.com/help.php).
+- Video instructions on how to create databases and tables in HeidiSQL can be viewed [here](https://www.youtube.com/watch?v=mRlXm04S_RY).
+
+## Instructions
+
+1. Create a new account for your bot *(you can technically use your own Twitch but not ideal)*
+2. Obtain an OAUTH token for your bot's account from [here](https://twitchapps.com/tmi/).  *(Please note I don't necessarily endorse that site or know it's security but I know it works for me and others recommend it.)*
+3. Download the latest release version of this repo
+4. Extract the release, and then any of the following steps will be done inside the release directory
+5. Create a ***.env*** file containing the following information ***Don't use dashes in any name for your databases. I found that out the hard way. Use underscores instead.***
+
+```text
+TWITCH_USERNAME = <Name of the chatbot>
+TWITCH_OAUTH = <The oauth token you generated earlier>
+TWITCH_CHANNELS = <The name of the channel for the bot to join>
+TWITCH_PREFIX = <The character you want your bot to recognize>
+DATABASE_HOST = <Where you're database lives>
+DATABASE_USER = <User to authenticate to the DB>
+DATABASE_PASSWORD = <Password to authenticate to the DB>
+DATABASE_NAME = <The name of your database>
+```
+
+NOTE: If you would like the bot to join multiple streams, in the `TWITCH_CHANNELS` make a comma-seperated list of each channel. For instance, if you would like the bot to join Stream1 and Stream2, the .env would look like `TWITCH_CHANNELS = Stream1, Stream2`
+
+5. Inside *app.js* Insert your new commands as new case statements within the switch
+
+```javascript
+switch (command) {
+    case 'yournewcommandhere':
+        // Your command behavior
+        break;
+}
+```
+
+6. Add the usage of your new command to the `getUsageInfo` function like this
+
+```javascript
+switch (command) {
+    case 'yournewcommandhere':
+      return "What your new command does. Usage: !yournewcommand <parameter1> <parameter2>. Example: \"!yournewcommand parameter1 parameter2\"" 
+}
+```
+
+7. Run your bot in the terminal or command line session using `node app.js`
---
+++ app.js
@@ -0,0 +1,236 @@
+//! TO RUN THE BOT: node app.js
+
+require('dotenv').config();
+const tmi = require('tmi.js');
+const mysql = require('mysql');
+
+// global var to use with sql queries
+var sqlError = null;
+// name of table in the database were saving things to
+var sqlTable = 'userinfo';
+
+// create DB connection
+var db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+});
+
+// Connect to DB
+db.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to database!');
+});
+
+
+
+// Create the channels to join list
+const channelsString = process.env.TWITCH_CHANNELS;
+const channelsArray = channelsString.replace(' ', '').split(',');
+
+// create the twitch tmi client
+var client = new tmi.Client({
+    options: { debug: true, messagesLogLevel: "info"},
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+
+    identity: {
+        username: `${process.env.TWITCH_USERNAME}`,
+        password: `${process.env.TWITCH_OAUTH}`
+    },
+
+    channels: channelsArray
+});
+
+client.connect().catch(console.error);
+incrementUserCums(client, 'gravitybzk', 'bazooka3');
+client.on('message', (channel, tags, message, self) => {
+    // Ignores chat messages from the bot
+    if (self) return;
+
+    // Get the first character (which is the prefix)
+    const messagePrefix = message.charAt(0);
+    // The command is the firt word of the message minus the first character
+    const command = message.split(' ')[0].slice(1).toLowerCase();
+    // Rest of the message is the input minues the command
+    const restOfMessage = message.split(' ').slice(1).join(' ');
+
+    // Ignore message if it does not contain the bot's prefix
+    if (!(messagePrefix === `${process.env.TWITCH_PREFIX}`)) {
+        // Does not have the bot's prefix in the first character; IGNORE
+        console.log('not a command');
+        return;
+    }
+
+    //* Commands list here
+    var commandList = ['usage', 'hey', 'cum', 'announce'];
+    console.log(channel);
+
+    const sender = tags.username;
+
+    switch (command) {
+        //! ANY USER INPUT TOUCHING THE DB NEEDS TO BE CLEANED mysql.escape()
+        case 'commands':
+            client.say(channel, `@${sender} the commands available for this channel are: ${commandList.join(', ')}`);
+            break;
+
+        case 'usage':
+            if (restOfMessage) {
+                const splitMessage = restOfMessage.split(' ');
+                if (splitMessage.length > 1) {
+                    // sent something like !usage blah blah
+                    client.say(channel, `@${sender} Too many parameters. Usage: ${getUsageInfo(command)}`);
+                }
+                else {
+                    // sent only one parameter good
+                    client.say(channel, `@${sender} Usage of command ${splitMessage[0]}: ${getUsageInfo(splitMessage[0])}`);
+                }
+            }
+            else {
+                // sent only !usage
+                client.say(channel, `@${sender} Must supply a command. Usage: ${getUsageInfo(command)}`);
+            }
+            break;
+
+        case 'hey':
+            client.say(channel, `@${sender} What's up yo!`);
+            break;
+
+        case 'cum':
+            if (restOfMessage) {
+                const splitMessage = restOfMessage.split(' ');
+                if (splitMessage.length > 1) {
+                    // Entered more than one command
+                    client.say(channel, `@${sender} We're sorry, but you can't cum on more than one person. Usage of ${command}: ${getUsageInfo(command)}`);
+                }
+                else {
+
+                }
+            }
+            else {
+                // nothing but command. just increment this user's cums
+                const userCums = incrementUserCums(sender);
+                if (userCums !== -1) {
+                    // succesfully cummed on chat
+                    client.say(channel, `borpaSpin @${sender} just cummed on chat! borpaSpin They have cummed a total of ${userCums} times. borpaSpin`);
+                }
+                else {
+                    client.say(channel, `Sorry @${sender}, we couldn't handle your cum right now. There may be a problem with the database.`);
+                }
+            }
+            break;
+
+        case 'announce':
+            client.say(channel, `/announce @${sender} wants to say that \"${restOfMessage}\"`);
+            break;
+
+        default:
+            client.say(channel, `@${sender} the command ${command} was not found. Use !commands to see a list of all commands for this bot.`);
+    }
+});
+
+/**
+ *  Increments the number of cums for a given user.
+ *  @async
+ *  @param  {string}    channel    The channel name
+ *  @param  {string}    username   The username to increment
+ *  @return {number}               The current number of cums for the user | -1 if an error occurs
+ */ 
+async function incrementUserCums(channel, username) {
+    try {
+        const query = `SELECT * FROM ${sqlTable} WHERE username=${mysql.escape(username)}`;
+        const userInfo = await queryDB(query);
+        var currentCums;
+
+        if (userInfo[0]) {
+            // The user already exists; just update cums
+            const userCums = userInfo[0].cums;
+            currentCums = userCums + 1
+            const updateQuery = `UPDATE ${sqlTable} SET cums=${currentCums} WHERE username=${mysql.escape(username)}`;
+            queryDB(updateQuery);
+        }
+        else {
+            // The user is not in the database, create with one cums
+            currentCums = 1
+            const createQuery = `INSERT INTO ${sqlTable} (username, cums) VALUES (${mysql.escape(username)}, ${currentCums})`;
+            queryDB(createQuery);
+        }
+
+        tmiClient.say(channel, `borpaSpin @${username} just came all over the chat! They have cum a total of ${currentCums} times.`);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+/**
+ *  Executes a query and returns the result
+ *  @param   {string}    query   the query to execute on the database
+ *  @returns {Promise}           promise containing the execution
+ */ 
+function queryDB(query) {
+    return new Promise((resolve, reject) => {
+        db.query(query + ';', (err, result) => {
+            if (err) {
+                sqlError = err;
+                console.log('Error while communicating with database: ' + err);
+                return reject();
+            }
+
+            console.log('Query: \"' + query + '\" executed successfully.');
+            return resolve(result);
+        });
+    });
+}
+
+function getUsageInfo(command) {
+    switch (command) {
+        case 'commands':
+            return "Gets a list of the bot's commands. Usage: \"!commands\"";
+
+        case 'usage':
+            return "Returns the usage info for a command. Usage: !usage <(string) command>. Example: \"!usage commands\"";
+
+        case 'hey':
+            
+            break;
+
+        case 'cum':
+            
+            break;
+
+        case 'announce':
+            
+            break;
+
+        default:
+            return "No usage. Unknown command!";
+    }
+}
+
+process.stdin.resume();
+
+//! This was being called too early and closing the connection 
+// referenced from here: https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits
+function exitHandler(options) {
+    //Close the connection to the database
+    db.end((err) => {
+        if (err) {
+            console.log(`Error closing connection: ${err}`);
+            console.log(`Could not close connection to database.`);
+            return;
+        }
+
+        console.log('Database connection successfully closed.');
+    });
+
+    if (options.exit) {
+        process.exit();
+    }
+}
+
+process.on('exit', exitHandler.bind(null));
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
